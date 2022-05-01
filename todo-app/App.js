8c12e1e37aebe815661a642e6ea1f8c540ae3d42
Maxwel Fávaro de Oliveira <ana.manuellar@gmail.com>
2022-05-01 18:02:03 -0300

    first commit

---
+++ todo-app/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>TODO LIST</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fefefe',
+    backgroundImage: 'linear-gradient(#fc6c48 0%, #ef5081 100%)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+});
