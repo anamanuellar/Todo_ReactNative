5fc9f519816a7a197fd934739ecc2825def706b4
Maxwel Fávaro de Oliveira <ana.manuellar@gmail.com>
2022-05-01 18:12:03 -0300

    second commit

--- todo-app/App.js
+++ todo-app/App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Keyboard, ScrollView } from 'react-native';
+import TaskItem from './components/TaskItem';
+import TaskInputField from './components/TaskInputField'
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  }
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>TODO LIST</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {
+        tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <TaskItem index={index + 1} task={task} deleteTask={() => deleteTask(index)}/>
+            </View>
+          );
+        })
+      }
+      </ScrollView>
+      <TaskInputField addTask={addTask}/>
     </View>
   );
 }
@@ -25,5 +49,11 @@ const styles = StyleSheet.create({
     marginTop: 30,
     marginBottom: 10,
     marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
   },
 });
